c9bbb40d1437691fb5e8ffb8e5a05b1294a9439e
BuildTools <unconfigured@null.spigotmc.org>
2020-06-11 19:59:09 +0300

    1.4

--- src/main/java/fi/jokni/newplayer/NewPlayer.java
+++ src/main/java/fi/jokni/newplayer/NewPlayer.java
@@ -18,16 +18,15 @@ public final class NewPlayer extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         System.out.println(ChatColor.GREEN + "[NewPlayer] Loading NewPlayer...");
-        this.getConfig().options().copyDefaults(true);
-        this.saveConfig();
         Bukkit.getPluginManager().registerEvents(this, this);
+        this.getConfig();
+        this.saveDefaultConfig();
 
     }
 
     @Override
     public void onDisable() {
         System.out.println(ChatColor.RED + "[NewPlayer] Unloading NewPlayer...");
-        this.saveConfig();
     }
 
     private HashMap<UUID, Long> cooldown = new HashMap<UUID, Long>();
@@ -35,13 +34,13 @@ public final class NewPlayer extends JavaPlugin implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         String sound = this.getConfig().getString("sound");
-        String message = this.getConfig().getString("message");
-        String beeper = this.getConfig().getString("pc-beeper");
-        String everyjoin = this.getConfig().getString("every-join");
+        String message = this.getConfig().getString("notify-join");
+        boolean beeper = this.getConfig().getBoolean("pc-beeper");
+        boolean everyjoin = this.getConfig().getBoolean("every-join");
         Player p = e.getPlayer();
         for (Player p2 : Bukkit.getOnlinePlayers()) {
             if (p2.hasPermission("newplayer.notify")) {
-                if (getConfig().get(beeper) == "true") {
+                if (beeper == true) {
                     if (!p.hasPlayedBefore()) {
                         if (cooldown.isEmpty()) {
                             cooldown.put(p.getUniqueId(), System.currentTimeMillis());
@@ -49,22 +48,23 @@ public final class NewPlayer extends JavaPlugin implements Listener {
                             p2.sendMessage(message);
                             Toolkit.getDefaultToolkit().beep();
                             Bukkit.getScheduler().runTaskLaterAsynchronously(this, () -> cooldown.clear(), 40);
+                        } else {
+                            return;
                         }
-                        return;
                     } else {
-                        if (getConfig().get(everyjoin) == "true") {
+                        if (everyjoin == true) {
                             p2.playSound(p2.getLocation(), Sound.valueOf(sound), 2F, 1F);
                             p2.sendMessage(message);
-                        } else {
-                            if (!p.hasPlayedBefore()) {
-                                p2.playSound(p2.getLocation(), Sound.valueOf(sound), 2F, 1F);
-                                p2.sendMessage(message);
-                            } else {
-                                if (getConfig().get(everyjoin) == "true") {
-                                    p2.playSound(p2.getLocation(), Sound.valueOf(sound), 2F, 1F);
-                                    p2.sendMessage(message);
-                                }
-                            }
+                        }return;
+                    }
+                } else {
+                    if (everyjoin == true) {
+                        p2.playSound(p2.getLocation(), Sound.valueOf(sound), 2F, 1F);
+                        p2.sendMessage(message);
+                    } else {
+                        if (!p.hasPlayedBefore()) {
+                            p2.playSound(p2.getLocation(), Sound.valueOf(sound), 2F, 1F);
+                            p2.sendMessage(message);
                         }
                     }
                 }
