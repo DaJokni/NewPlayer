c7b3101644097f6b469c9744d8b407f89010ddf8
BuildTools <unconfigured@null.spigotmc.org>
2020-06-11 15:57:33 +0300

    1.3

--- src/main/java/fi/jokni/newplayer/NewPlayer.java
+++ src/main/java/fi/jokni/newplayer/NewPlayer.java
@@ -9,6 +9,10 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.awt.*;
+import java.util.HashMap;
+import java.util.UUID;
+
 public final class NewPlayer extends JavaPlugin implements Listener {
 
     @Override
@@ -26,16 +30,43 @@ public final class NewPlayer extends JavaPlugin implements Listener {
         this.saveConfig();
     }
 
+    private HashMap<UUID, Long> cooldown = new HashMap<UUID, Long>();
+
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         String sound = this.getConfig().getString("sound");
         String message = this.getConfig().getString("message");
+        String beeper = this.getConfig().getString("pc-beeper");
+        String everyjoin = this.getConfig().getString("every-join");
         Player p = e.getPlayer();
         for (Player p2 : Bukkit.getOnlinePlayers()) {
-            if (!p.hasPlayedBefore()) {
-                if (p2.hasPermission("newplayer.notify")) {
-                    p2.playSound(p2.getLocation(), Sound.valueOf(sound), 2F, 1F);
-                    p2.sendMessage(message);
+            if (p2.hasPermission("newplayer.notify")) {
+                if (getConfig().get(beeper) == "true") {
+                    if (!p.hasPlayedBefore()) {
+                        if (cooldown.isEmpty()) {
+                            cooldown.put(p.getUniqueId(), System.currentTimeMillis());
+                            p2.playSound(p2.getLocation(), Sound.valueOf(sound), 2F, 1F);
+                            p2.sendMessage(message);
+                            Toolkit.getDefaultToolkit().beep();
+                            Bukkit.getScheduler().runTaskLaterAsynchronously(this, () -> cooldown.clear(), 40);
+                        }
+                        return;
+                    } else {
+                        if (getConfig().get(everyjoin) == "true") {
+                            p2.playSound(p2.getLocation(), Sound.valueOf(sound), 2F, 1F);
+                            p2.sendMessage(message);
+                        } else {
+                            if (!p.hasPlayedBefore()) {
+                                p2.playSound(p2.getLocation(), Sound.valueOf(sound), 2F, 1F);
+                                p2.sendMessage(message);
+                            } else {
+                                if (getConfig().get(everyjoin) == "true") {
+                                    p2.playSound(p2.getLocation(), Sound.valueOf(sound), 2F, 1F);
+                                    p2.sendMessage(message);
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
